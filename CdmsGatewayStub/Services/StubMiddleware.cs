4ee504c91874f5ba34c38dfb91b240d5dad4c2ee
Ian Shimmings <ian.shimmings@hotmail.com>
2024-11-08 15:30:34 +0000

    Added tests and support for routed and forked routed

--- CdmsGatewayStub/Services/StubMiddleware.cs
+++ CdmsGatewayStub/Services/StubMiddleware.cs
@@ -2,6 +2,7 @@ using System.Net;
 using System.Net.Mime;
 using System.Text;
 using CdmsGatewayStub.Utils;
+using Microsoft.Extensions.Primitives;
 using ILogger = Serilog.ILogger;
 
 namespace CdmsGatewayStub.Services;
@@ -25,6 +26,7 @@ public class StubMiddleware(RequestDelegate next, IStubActions stubActions, ILog
 
         context.Response.StatusCode = (int)HttpStatusCode.OK;
         context.Response.Headers.Date = DateTimeOffset.UtcNow.ToString("R");
+        context.Response.Headers.Append("x-requested-path", new StringValues(context.Request.Path));
         
         var contentType = context.Request.ContentType ?? "";
         context.Response.ContentType = contentType;
