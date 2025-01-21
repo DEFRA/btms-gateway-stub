53d9aebaabe121e39311d654211fd602c1373c67
Ian Shimmings <ian.shimmings@hotmail.com>
2025-01-21 10:49:38 +0000

    Modified startup and config to match gateway and prepare for swagger endpoints

---
+++ BtmsGatewayStub/Config/Swagger.cs
@@ -0,0 +1,8 @@
+namespace BtmsGatewayStub.Config;
+
+public static class Swagger
+{
+    public static bool IsSwaggerEnabled(this WebApplicationBuilder builder) => builder.IsDevMode() || builder.Configuration.GetValue<bool>("EnableSwagger");
+    
+    public static bool IsSwaggerEnabled(this WebApplication app) => app.IsDevMode() || app.Configuration.GetValue<bool>("EnableSwagger");
+}
--- BtmsGatewayStub/Program.cs
+++ BtmsGatewayStub/Program.cs
@@ -1,13 +1,12 @@
 using Serilog;
+using Serilog.Core;
 using System.Diagnostics.CodeAnalysis;
-using BtmsGatewayStub.Utils.Logging;
+using BtmsGatewayStub.Config;
 using BtmsGatewayStub.Services;
 using BtmsGatewayStub.Utils;
-using OpenTelemetry;
-using OpenTelemetry.Metrics;
-using OpenTelemetry.Trace;
-using Serilog.Core;
-using ILogger = Serilog.ILogger;
+using BtmsGatewayStub.Utils.Logging;
+using Microsoft.OpenApi.Models;
+using Environment = System.Environment;
 
 var app = CreateWebApplication(args);
 await app.RunAsync();
@@ -19,7 +18,15 @@ static WebApplication CreateWebApplication(string[] args)
 
     ConfigureWebApplication(builder);
 
-    var app = BuildWebApplication(builder);
+    ConfigureSwaggerBuilder(builder);
+
+    var app = builder.Build();
+
+    app.UseMiddleware<StubMiddleware>();
+    app.MapHealthChecks("/health");
+    // app.UseCheckRoutesEndpoints();
+
+    ConfigureSwaggerApp(app);
 
     return app;
 }
@@ -30,28 +37,11 @@ static void ConfigureWebApplication(WebApplicationBuilder builder)
     builder.Configuration.AddEnvironmentVariables();
     builder.Configuration.AddIniFile("Properties/local.env", true);
 
-    builder.Services.AddOpenTelemetry()
-        .WithMetrics(metrics =>
-        {
-            metrics.AddRuntimeInstrumentation()
-                   .AddMeter(
-                       "Microsoft.AspNetCore.Hosting",
-                       "Microsoft.AspNetCore.Server.Kestrel",
-                       "System.Net.Http");
-        })
-        .WithTracing(tracing =>
-        {
-            tracing.AddAspNetCoreInstrumentation()
-                   .AddHttpClientInstrumentation();
-        })
-        .UseOtlpExporter();
-
     var logger = ConfigureLogging(builder);
-    
-    // Load certificates into Trust Store - Note must happen before Mongo and Http client connections
-    builder.Services.AddCustomTrustStore(logger);
 
-    ConfigureEndpoints(builder);
+    builder.Services.AddCustomTrustStore(logger);
+    builder.Services.AddHealthChecks();
+    builder.AddServices(logger);
 }
 
 [ExcludeFromCodeCoverage]
@@ -61,34 +51,33 @@ static Logger ConfigureLogging(WebApplicationBuilder builder)
     var loggerConfiguration = new LoggerConfiguration()
         .ReadFrom.Configuration(builder.Configuration)
         .Enrich.With<LogLevelMapper>()
-        .Enrich.WithProperty("service.version", Environment.GetEnvironmentVariable("SERVICE_VERSION"))
-        .WriteTo.OpenTelemetry(options =>
-        {
-            options.LogsEndpoint = builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"];
-            options.ResourceAttributes.Add("service.name", "btms-gateway-stub");
-        });
+        .Enrich.WithProperty("service.version", Environment.GetEnvironmentVariable("SERVICE_VERSION"));
     
     var logger = loggerConfiguration.CreateLogger();
     builder.Logging.AddSerilog(logger);
-    builder.Services.AddSingleton<ILogger>(logger);
     logger.Information("Starting application");
     return logger;
 }
 
 [ExcludeFromCodeCoverage]
-static void ConfigureEndpoints(WebApplicationBuilder builder)
+static void ConfigureSwaggerBuilder(WebApplicationBuilder builder)
 {
-    builder.Services.AddHealthChecks();
+    if (builder.IsSwaggerEnabled())
+    {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("public-v0.1", new OpenApiInfo { Title = "TDM Public API", Version = "v1" }); });
+    }
 }
 
 [ExcludeFromCodeCoverage]
-static WebApplication BuildWebApplication(WebApplicationBuilder builder)
+static void ConfigureSwaggerApp(WebApplication app)
 {
-    var app = builder.Build();
-
-    app.UseMiddleware<StubMiddleware>();
-   
-    app.MapHealthChecks("/health");
-
-    return app;
+    if (app.IsSwaggerEnabled())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/public-v0.1/swagger.json", "public");
+        });
+    }
 }
