1f2ca8f150be612958007bd6568b4f913d692ef7
Ian Shimmings <ian.shimmings@hotmail.com>
2025-01-22 11:10:06 +0000

    Moved gateway url into config

--- BtmsGatewayStub/Services/AlvsEndpoints.cs
+++ BtmsGatewayStub/Services/AlvsEndpoints.cs
@@ -7,9 +7,11 @@ using Swashbuckle.AspNetCore.Annotations;
 namespace BtmsGatewayStub.Services;
 
 [ApiController, Route(Path)]
-public class AlvsEndpoints(IHttpClientFactory httpClientFactory) : ControllerBase
+public class AlvsEndpoints(IHttpClientFactory httpClientFactory, IConfiguration configuration) : ControllerBase
 {   
     public const string Path = "alvs-simulator";
+
+    private readonly string _gatewayUrl = configuration["gatewayUrl"]!;
     
     [HttpPost("decision-notification")]
     [Consumes(MediaTypeNames.Text.Plain), Produces(MediaTypeNames.Text.Plain)]
@@ -17,7 +19,7 @@ public class AlvsEndpoints(IHttpClientFactory httpClientFactory) : ControllerBas
     public async Task<ActionResult> SendDecisionNotification([FromBody] string content)
     {
         var client = httpClientFactory.CreateClient(Proxy.ProxyClient);
-        var response = await client.PostAsync($"http://btms-gateway.localtest.me:3091/alvs-cds/ws/CDS/defra/alvsclearanceinbound/v1", new StringContent(content, Encoding.UTF8, MediaTypeNames.Application.Soap));
+        var response = await client.PostAsync($"{_gatewayUrl}/alvs-cds/ws/CDS/defra/alvsclearanceinbound/v1", new StringContent(content, Encoding.UTF8, MediaTypeNames.Application.Soap));
         return new ObjectResult(await response.Content.ReadAsStringAsync()) { StatusCode = (int)response.StatusCode };
     }
 }
