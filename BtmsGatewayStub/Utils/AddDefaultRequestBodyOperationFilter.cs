cbf927947c1bf26d5bd1dbf25354dc08140a07ba
Ian Shimmings <ian.shimmings@hotmail.com>
2025-01-23 12:59:39 +0000

    More test fixes

--- BtmsGatewayStub/Utils/AddDefaultRequestBodyOperationFilter.cs
+++ BtmsGatewayStub/Utils/AddDefaultRequestBodyOperationFilter.cs
@@ -15,8 +15,8 @@ public class AddDefaultRequestBodyOperationFilter : IOperationFilter
         
         operation.RequestBody.Content["text/plain"].Example = context.MethodInfo.Name switch
         {
-            nameof(AlvsEndpoints.SendDecisionNotification) => new OpenApiString(File.ReadAllText(Path.Combine(ExamplesPath, "DecisionNotification.xml"))),
             nameof(ALVS_Simulator.SendDecisionNotification) => new OpenApiString(File.ReadAllText(Path.Combine(ExamplesPath, "DecisionNotification.xml"))),
+            nameof(CDS_Simulator.SendClearanceRequest) => new OpenApiString(File.ReadAllText(Path.Combine(ExamplesPath, "ClearanceRequest.xml"))),
             
             _ => new OpenApiString(string.Empty)
         };
